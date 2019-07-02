6a832e0f7e11406c06fa905575349f0119b2273f
eyzzzu <eyzzzu@gmail.com>
2019-07-02 17:10:45 +0300

    removed extra get and set and formatted cashier's check

--- src/com/homework/Fruit.java
+++ src/com/homework/Fruit.java
@@ -23,7 +23,6 @@ public class Fruit {
     public double getPrice() {
         return pricePerKg * weight / 1000; // округлить до сотых
     }
-
     @Override
     public String toString() {
         final StringBuffer sb = new StringBuffer("Fruit{");
