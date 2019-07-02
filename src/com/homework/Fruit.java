9de399bd47965c0426f9e4aeee899b593a042339
eyzzzu <eyzzzu@gmail.com>
2019-07-02 16:28:49 +0300

    removed extra get and set

--- src/com/homework/Fruit.java
+++ src/com/homework/Fruit.java
@@ -20,38 +20,6 @@ public class Fruit {
         return pricePerKg * weight / 1000; // округлить до сотых
     }
 
-    public FruitType getType() {
-        return type;
-    }
-
-    public void setType(FruitType type) {
-        this.type = type;
-    }
-
-    public FruitColor getColor() {
-        return color;
-    }
-
-    public void setColor(FruitColor color) {
-        this.color = color;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
-
-    public double getPricePerKg() {
-        return pricePerKg;
-    }
-
-    public void setPricePerKg(double pricePerKg) {
-        this.pricePerKg = pricePerKg;
-    }
-
     @Override
     public String toString() {
         final StringBuffer sb = new StringBuffer("Fruit{");
